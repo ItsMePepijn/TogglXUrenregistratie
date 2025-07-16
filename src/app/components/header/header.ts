d865b7e8697265c3fe2f8776ab7a88de67de6692
Pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2025-07-16 20:52:20 +0200

    Header styling

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { MenuItem } from 'primeng/api';
+import { AsyncPipe } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [MenuModule, ButtonModule, AsyncPipe],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  protected readonly items: MenuItem[] = [
+    {
+      label: 'Logout',
+      icon: 'pi pi-sign-out',
+      command: () => {
+        this.userService.logout();
+        this.route.navigate(['/login']);
+      },
+    },
+  ];
+
+  constructor(
+    protected readonly userService: UserService,
+    private readonly route: Router
+  ) {}
+}
