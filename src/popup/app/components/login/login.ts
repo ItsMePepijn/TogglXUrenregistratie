4ad0d8ce31039f71a8104636c052daa8efec67fa
Pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2025-09-16 21:58:16 +0200

    Log in using api token

--- src/popup/app/components/login/login.ts
+++ src/popup/app/components/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -8,7 +8,7 @@ import {
 } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
-import { BehaviorSubject, take } from 'rxjs';
+import { BehaviorSubject, filter, map, switchMap, take, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -18,6 +18,7 @@ import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-login',
@@ -37,10 +38,11 @@ import { MessageModule } from 'primeng/message';
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
   protected readonly loginForm = new FormGroup({
-    email: new FormControl<string | null>(null, [Validators.required]),
-    password: new FormControl<string | null>(null, [Validators.required]),
+    email: new FormControl<string | null>(null),
+    password: new FormControl<string | null>(null),
+    token: new FormControl<string | null>(null),
   });
 
   private readonly _loginError$ = new BehaviorSubject<string | null>(null);
@@ -49,10 +51,38 @@ export class Login {
   private readonly _isLoading$ = new BehaviorSubject<boolean>(false);
   protected readonly isLoading$ = this._isLoading$.asObservable();
 
+  private readonly _useToken$ = new BehaviorSubject<boolean>(false);
+  protected readonly useToken$ = this._useToken$.asObservable();
+
   constructor(
     private readonly userService: UserService,
     private readonly router: Router,
+    private readonly destroyRef: DestroyRef,
   ) {}
+
+  public ngOnInit() {
+    this.useToken$
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((useToken) => {
+        if (useToken) {
+          this.loginForm.controls.token.setValidators([Validators.required]);
+          this.loginForm.controls.email.clearValidators();
+          this.loginForm.controls.password.clearValidators();
+        } else {
+          this.loginForm.controls.token.clearValidators();
+          this.loginForm.controls.email.setValidators([Validators.required]);
+          this.loginForm.controls.password.setValidators([Validators.required]);
+        }
+
+        this.loginForm.controls.token.updateValueAndValidity();
+        this.loginForm.controls.email.updateValueAndValidity();
+        this.loginForm.controls.password.updateValueAndValidity();
+      });
+  }
+
+  protected setUseToken(useToken: boolean) {
+    this._useToken$.next(useToken);
+  }
 
   protected login() {
     this._loginError$.next(null);
@@ -61,15 +91,31 @@ export class Login {
       return;
     }
 
-    const data = this.loginForm.value;
-    if (!data.email || !data.password) return;
+    this.useToken$
+      .pipe(
+        take(1),
+        map((useToken) => {
+          const data = this.loginForm.value;
 
-    this._isLoading$.next(true);
-    this.userService
-      .login(data.email, data.password)
-      .pipe(take(1))
+          return useToken
+            ? {
+                email: data.token ?? null,
+                password: 'api_token',
+              }
+            : {
+                email: data.email ?? null,
+                password: data.password ?? null,
+              };
+        }),
+        filter((data) => data.email !== null && data.password !== null),
+        tap(() => {
+          this._isLoading$.next(true);
+        }),
+        switchMap((data) => {
+          return this.userService.login(data.email!, data.password!);
+        }),
+      )
       .subscribe((result) => {
-        console.log(result);
         if (result.success) {
           this.router.navigate(['']);
         } else {
